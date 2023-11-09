1559c5aecb03fd58f323aa6589aa94887bde9eba
Rahim sikder <arsikder323@gmail.com>
2023-11-10 00:35:57 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,12 @@
+const country ='Bangladesh';
+const age = 29;
+const isIndependant = true;
+const student = {id:120, class:11, name:'rahim'};
+const friends =[12, 13,14, 15, 16,17,];
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependant);
+console.log(typeof student);
+// check arrray isArray
+console.log(Array.isArray(friends));
+console.log(friends.includes(18));
